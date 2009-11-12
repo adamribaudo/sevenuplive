c4a914352845303fcf824e761e2767abc1c2a031
chrismlloyd@gorge.net <chrismlloyd@gorge.net@c993f61c-f268-11dd-89c5-9df526a60542>
2009-11-12 02:26:58 +0000

    Ok, midiin is working on the SevenUp4Live component.
    ch 1-8 are for CLIP launch feedback...which seems a bit useless at the moment.
    ch 9-15 are for external instrument. This works.

--- trunk/src/mtn/sevenuplive/m4l/M4LMidiIn.java
+++ trunk/src/mtn/sevenuplive/m4l/M4LMidiIn.java
@@ -1,5 +1,14 @@
 package mtn.sevenuplive.m4l;
 
 public interface M4LMidiIn {
+	
+	public final static String NOTE = "NOTE";
+	public final static String CC = "CC";
+	
+	public void sendNoteOn(Note note);
+	
+	public void sendNoteOff(Note note);
+	
+	public void sendController(M4LController controller);
 
 }

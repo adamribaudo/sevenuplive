94bfce84760f4bfef10b39f0b04b7b38c3503142
chrismlloyd <chrismlloyd@gorge.net>
2009-11-12 01:54:39 +0000

    Ok, Midi IN works now and so does external instrument.
    It's a bit unintuitive though since channels 1-8 are listening for clip launch feedback.
    And external intruments start in channel 9. 
    It'd be better to get rid of clip launch feedback and listen for
    external instruments starting on ch 1

--- src/mtn/sevenuplive/m4l/Note.java
+++ src/mtn/sevenuplive/m4l/Note.java
@@ -6,11 +6,16 @@ public class Note {
 	private int vel;
 	private int dur;
 	private int status;
-
+	
 	public Note(int pitch, int vel, int dur) {
+		this(pitch, vel, dur, 144);
+	}
+	
+	public Note(int pitch, int vel, int dur, int status) {
 		this.pitch = pitch;
 		this.vel = vel;
 		this.dur = dur;
+		this.status = status;
 	}
 	
 	public int getPitch(){ 
@@ -21,11 +26,11 @@ public class Note {
 		return vel;
 	}
 	
-	public int getStatus() {
-		return status;
-	}
-	
 	public int getLength() {
 		return dur;
+	}
+	
+	public int getStatus() {
+		return status;
 	}
 }

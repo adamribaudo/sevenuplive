1b84e1c1565654ed1155ed4a5cf61ff71f20b870
chrismlloyd <chrismlloyd@gorge.net>
2009-11-15 18:39:45 +0000

    Integrate from trunk

--- src/mtn/sevenuplive/modes/PatternizerView.java
+++ src/mtn/sevenuplive/modes/PatternizerView.java
@@ -1,9 +1,30 @@
+/*
+	Copyright 2009 Adam Ribaldo 
+	 
+	Developed by Adam Ribaldo, Chris Lloyd
+    
+    This file is part of SevenUpLive.
+    http://www.makingthenoise.com/sevenup/
+
+    SevenUpLive is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SevenUpLive is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with SevenUpLive.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package mtn.sevenuplive.modes;
 
 /***
  * The patternizer view allows different windows into the same patternizer.  Each grid can view a different pattern at the same time.
  * @author Adam Ribaudo
- *
  */
 public class PatternizerView extends Mode {
 	
